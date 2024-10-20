7930e8df5125515adefcdd6f068d99505a79b17b
Harish Pant <pantharish120@gmail.com>
2024-10-20 15:50:34 +0545

    :zap: custom command setup

--- backend/routes/console.php
+++ backend/routes/console.php
@@ -2,7 +2,17 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
-})->purpose('Display an inspiring quote')->hourly();
+})->purpose('Display an inspiring quote')->dailyAt('12:00');
+
+Schedule::command('sync:orders')->everyMinute()
+    ->onFailure(function () {
+        Log::error('Failed to sync orders from Woocommerce.');
+    })
+    ->onSuccess(function () {
+        Log::info('Orders synced successfully.');
+    });
