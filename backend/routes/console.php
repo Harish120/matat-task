00ba1d8bf495e10b387e1076a27ab45b1db84b07
Harish Pant <pantharish120@gmail.com>
2024-10-21 20:53:01 +0545

    :fire: command running time fixes

--- backend/routes/console.php
+++ backend/routes/console.php
@@ -7,9 +7,9 @@ use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
-})->purpose('Display an inspiring quote')->dailyAt('12:00');
+})->purpose('Display an inspiring quote');
 
-Schedule::command('sync:orders')->everyMinute()
+Schedule::command('sync:orders')->dailyAt('12:00')
     ->onFailure(function () {
         Log::error('Failed to sync orders from Woocommerce.');
     })
